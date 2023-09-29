226db6a258a4b378e158942c561dea7f6858f433
Peter Santing <p.santing@plusautomatisering.nl>
2023-09-29 15:37:44 +0200

    Update mapper_001.cpp

--- bus/rom/mappers/mapper_001.cpp
+++ bus/rom/mappers/mapper_001.cpp
@@ -177,14 +177,14 @@ void mmc1_vrom::update_banks(mmc1_state &state) {
 	switch (state.chr_bank_mode) {
 	case MMC1_CHR_8K_MODE:
 		bank = state.chrbank0 >> 1;
-		chr0000 = &romdata[(bank << 13)%romsize]; // 8 kB blocks.
-		chr1000 = &romdata[(bank << 13)%romsize | 0x1000];
+		chr0000 = &romdata[(bank << 13)%(romsize-4096)]; // 8 kB blocks.
+		chr1000 = &romdata[(bank << 13)%(romsize-4096) | 0x1000];
 		break;
 	case MMC1_CHR_4K_MODE:
 		bank = state.chrbank0;
-		chr0000 = &romdata[(bank << 12)%romsize]; // 4kB blocks.
+		chr0000 = &romdata[(bank << 12)%(romsize-4096)]; // 4kB blocks.
 		bank = state.chrbank1;
-		chr1000 = &romdata[(bank << 12)%romsize]; // 4kB blocks.
+		chr1000 = &romdata[(bank << 12)%(romsize-4096)]; // 4kB blocks.
 		break;
 	}
 }
